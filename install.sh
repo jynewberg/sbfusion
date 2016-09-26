ee7dae0d32f99bb637958292a0b547593805f2e1
Justin Y. Newberg <newberg@gmail.com>
2016-09-26 13:49:41 -0500

    Added attribute

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ export PYTHONPATH=$pylibdir:$PYTHONPATH
 mkdir -p $pylibdir
 
 
-echo "Defining transposon vector sequence"
+echo "Defining transposon vector sequence, modified from US20060026699 A1 (http://appft1.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PG01&p=1&u=/netahtml/PTO/srchnum.html&r=1&f=G&l=50&s1=20060026699.PGNR.)"
 echo ">onc2
 ccattcgccattcaggctgcgcaactgttgggaagggcgatcggtgcggg
 cctcttcgctattacgccagctggcgaaagggggatgtgctgcaaggcga
